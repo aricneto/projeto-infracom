e5ca8957208d984f7e1e5f5da551aa03b2f1e5fd
Ariovaldo Neto <a.neto@strategiesnorth.ca>
2023-07-31 16:04:10 -0300

    implementado transferencia de arquivo .txt

--- cliente.py
+++ cliente.py
@@ -1,23 +1,40 @@
-'''
+from common import Socket
+
+"""
 Cliente UDP
 
 Equipe: acn2
-'''
-
-from common import Socket
-
+"""
 
 client = Socket()
 print("Bem vindo ao transmissor de mensagens 3000\nDigite CTRL+X para sair")
+print("Comandos disponiveis:")
+print("> arquivo  | arq     : enviar arquivo")
+print("> mensagem | msg     : enviar mensagem")
+print("> shutdown | sdw     : desligar servidor")
+print("> exit     | ext     : sair do programa\n\n")
 
 while True:
-    msg = input()
+    msg = input("> ")
 
-    # terminar o programa caso CTRL+X seja apertado
-    if msg == "\x18": 
-        break
-
-    client.sendUDP(msg.encode())
+    match msg:
+        case "exit" | "\x18" | "ext":
+            break
+        case "mensagem" | "msg":
+            mensagem = input("Digite sua mensagem:\n")
+            client.sendUDP(mensagem.encode())
+        case "arquivo" | "arq":
+            filename = input("Digite o nome do arquivo:\n")
+            try:
+                arquivo = open(filename, "r")
+                data = arquivo.read()
+                if not data:
+                    break
+                while data:
+                    client.sendUDP(str(data).encode())
+                    data = arquivo.read()
+                arquivo.close()
+            except IOError:
+                print("Nome de arquivo inválido!")
 
 client.sock.close()
-
--- common.py
+++ common.py
@@ -1,5 +1,10 @@
 import socket
 
+'''
+Funções comuns a serem usadas pelos sockets
+
+Equipe: acn2
+'''
 
 class Socket: 
     def __init__(self, sock=None, host="localhost", port=5000, buffer_size=1024, server=False):
@@ -7,18 +12,18 @@ class Socket:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         else:
             self.sock = sock
-        self.dest = (host, port)
+        self.ip = (host, port)
         self.buffer_size = buffer_size
         
         if server: # se for um servidor, devemos chamar bind no endereço 
-            self.sock.bind(self.dest)
+            self.sock.bind(self.ip)
 
     def sendUDP(self, msg):
         total_sent = 0
         MSGLEN = len(msg)
         while total_sent < MSGLEN: # enquanto a mensagem ainda não foi completamente enviada
             # parcelar a mensagem em pacotes tamanho buffer_size
-            total_sent += self.sock.sendto(msg[total_sent:total_sent + self.buffer_size], self.dest)
+            total_sent += self.sock.sendto(msg[total_sent:total_sent + self.buffer_size], self.ip)
             print(f"> Bytes enviados: {total_sent}")
 
     def receiveUDP(self):
