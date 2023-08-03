ff9f9aef1ff04afd6a9bc9ee028e24cb7c64c96e
Ariovaldo Neto <a.neto@strategiesnorth.ca>
2023-08-02 21:18:52 -0300

    adicionado timeout na transmissao

--- servidor.py
+++ servidor.py
@@ -14,17 +14,22 @@ header = ""
 
 while True:
     if recebendo:
+        server.sock.settimeout(5)
         filename = "output/output.txt"
-        with open(filename, "wb") as new_file:
-            msg_size = 0
-            while True:
-                msg, _ = server.receiveUDP()
-                msg_size += len(msg)
-                new_file.write(msg)
-                
-                if msg_size == int(header[2]):
-                    break
-        print(f"Arquivo salvo: {filename}")
+        try:
+            with open(filename, "wb") as new_file:
+                msg_size = 0
+                while True:
+                    msg, _ = server.receiveUDP()
+                    msg_size += len(msg)
+                    new_file.write(msg)
+                    
+                    if msg_size == int(header[2]):
+                        break
+                print(f"Arquivo salvo: {filename}")
+        except TimeoutError:
+            print ("Erro na transmissão de arquivos.")
+        server.sock.settimeout(None)
         recebendo = False
 
     if not recebendo:
