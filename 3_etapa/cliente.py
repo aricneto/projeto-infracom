b71c35d9f4cfc929bd53d451872fbfa30df70e94
Ariovaldo Neto <a.neto@strategiesnorth.ca>
2023-09-21 23:37:39 -0300

    start 3 etapa

---
+++ 3_etapa/cliente.py
@@ -0,0 +1,69 @@
+from sender import Sender
+from receiver import Receiver
+from common import Socket
+from os.path import basename
+import os.path
+import threading
+
+"""
+Cliente UDP
+
+"""
+
+client = Socket(port=1337)
+sender = Sender(socket=client)
+receiver = Receiver(socket=client)
+
+# inicializar cliente
+CLIENT_DIR = "files_client"
+
+# definir servidor para onde vao ser enviados os arquivos
+server_ip = "localhost"
+server_port = 5000
+server_address = (server_ip, server_port)
+
+# inicializar pasta cliente
+if not os.path.exists(CLIENT_DIR):
+    os.makedirs(CLIENT_DIR)
+
+def main():
+    listen_thread = threading.Thread(target=listen)
+    send_thread = threading.Thread(target=send)
+    rcv_thread = threading.Thread(target=receive)
+    
+    listen_thread.start()
+    send_thread.start()
+    rcv_thread.start()
+    
+    listen_thread.join()
+    send_thread.join()
+    rcv_thread.join()
+    
+
+def listen():
+    while True:
+        header, packet, address = client.rdt_rcv()
+        sender.incoming_pkt = (header, packet, address)
+        receiver.incoming_pkt = (header, packet, address)
+
+def receive():
+    packet, address = None, []
+
+    while True:
+        if packet is None:
+            packet = receiver.wait_for_packet()
+        else:
+            print(f"Novo pacote: {packet}")
+            packet = None
+
+def send():
+    while True:
+        msg = input("Digite um comando:  ")
+
+        match msg:
+            case "bye":
+                break
+            case _:
+                sender.rdt_send(msg, server_address)
+
+main()
---
+++ 3_etapa/todo.md
@@ -0,0 +1,11 @@
+# Todo
+
+| Funcionalidade                        |  Comando                            |  |
+|---------------------------------------|-------------------------------------|--|
+| Conectar à sala                       |  hi, meu nome eh <nome_do_usuario>  |  |
+| Sair da sala                          |  bye                                |  |
+| Exibir lista de usuários do chat      |  list                               |  |
+| Exibir lista de amigos                |  mylist                             |  |
+| Adicionar usuário à lista de amigos   |  addtomylist <nome_do_usuario>      |  |
+| Remover usuário da lista de amigos    |  rmvfrommylist <nome_do_usuario>    |  |
+| Banir usuário da sala                 |  ban <nome_do_usuario>              |  |
